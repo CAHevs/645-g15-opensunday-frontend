b42ea68bafbe37d5298cf84d502ce46f51db724f
Alexandre <alex@MacBook-Pro-de-Alexandre.local>
2020-10-09 15:06:26 +0200

    UserForm, browserRouter for map and userform

--- src/App.js
+++ src/App.js
@@ -4,12 +4,44 @@ import { useAuth0 } from "@auth0/auth0-react";
 import request from "./utils/request";
 import endpoints from "./endpoints";
 import Loading from "./components/Loading";
-import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Link, Switch, Route, Redirect } from "react-router-dom";
 import LocationDetails from "./pages/LocationDetails";
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css'
+import UserForm from "./UserForm"
+
 
 function App() {
   let [locations, setLocations] = useState([]);
 
+  const [selectedDate, setSelectedDate] = useState(null);
+
+  let [user, setUsers] = useState([]);
+
+  //Get the city from the user's localization
+  let city = "Sion";
+
+  {
+    // const sunHolidDays = [{date: '20/10'}, {date: '21/10'},
+    // {date: '22/10'}, {date: '23/10'},
+    // {date: '24/10'}, {date: '24/10'}]
+
+    //list of sundays and holidays
+    // const holiSunday = date => {
+    //   const day = getDay(date);
+    //   return day ;
+    // };
+
+
+    //list of sundays and holidays
+    // const holiSunday = date => {
+    //   const day = getDay(date);
+    //   return day !== 0 && day !== 6;
+    // };
+  }
+
+
+  //Authentification with Auth0
   let {
     loading,
     loginWithRedirect,
@@ -18,7 +50,10 @@ function App() {
     isAuthenticated,
   } = useAuth0();
 
-  let handleLocationsClick = async (e) => {
+
+
+  //Login button with authentification
+  let handleLoginClick = async (e) => {
     e.preventDefault();
     let locations = await request(
       `${process.env.REACT_APP_SERVER_URL}${endpoints.locations}`,
@@ -26,17 +61,18 @@ function App() {
       loginWithRedirect
     );
 
-    if (locations && locations.length > 0) {
-      console.log(locations);
-      setLocations(locations);
-    }
+    //test if the user exists
+    // if (true) {
+    //   return <Redirect push to="./userform" />;
+
+    // }
+
   };
 
   let handleLogoutClick = async (e) => {
     e.preventDefault();
     /*
-    returnTo parameter is necessary because multiple
-    apps use the same authentication backend
+    returnTo parameter is necessary because multiple apps use the same authentication backend
     */
     logout({ returnTo: window.location.origin });
   };
@@ -45,20 +81,56 @@ function App() {
     return <Loading />;
   }
 
+
+
+  let addUser = (user) => {
+    setUsers((prevUsers) => [user, ...prevUsers]);
+  };
+
+
   return (
     <div className="App">
       <header className="App-header">
-        {isAuthenticated && (
+        {isAuthenticated ? (
+          /*If the user is authenticated*/
           <a
             className="App-link Logout-link"
             href="#"
             onClick={handleLogoutClick}
-          >
-            Logout
+          >Logout
           </a>
-        )}
-        <h1>OpenSunday</h1>
-        <br />
+
+        ) :
+          /*if the user isn't authenticated */
+          <a className="App-link Logout-link"
+            href="#"
+            onClick={handleLoginClick}
+          >Login
+          </a>
+        }
+
+        <h1>Welcome, login then select a town and a date</h1>
+
+        <input
+          //fieldRef={this.titleInputRef}
+          type="text"
+          name="city"
+          value={city}
+        // onChange={this.handleFormChange}
+        //placeholder="Title"
+        >
+        </input>
+
+
+        <DatePicker
+          selected={selectedDate}
+          onChange={date => setSelectedDate(date)}
+          filterDate={date => date.getDay() == 0}
+          //filterDate={sunHolidDays}
+          minDate={new Date()}
+          placeholderText="Select a sunday or holiday"
+        />
+
         <BrowserRouter>
           <Switch>
             <Route
@@ -66,31 +138,38 @@ function App() {
               exact
               render={() => (
                 <>
-                  <a
-                    className="App-link"
-                    href="#"
-                    onClick={handleLocationsClick}
-                  >
-                    Get Locations
-                  </a>
-                  {locations && locations.length > 0 && (
-                    <ul className="Locations-List">
-                      {locations.map((location) => (
-                        <li key={location.id}>
-                          <Link
-                            className="App-link"
-                            to={`/location/${location.id}`}
-                          >
-                            {location.name}
-                          </Link>
-                        </li>
-                      ))}
+
+                  {/* user != null
+                  ici on vérifie que le user exite dans notre db */}
+                  {isAuthenticated ? (
+                    <ul className="Map">
+                      <Link
+                        className="App-Map"
+                        to="/Map"
+                      //onClick={}
+                      >
+                        <button>
+                          map me
+                        </button>
+
+                      </Link>
                     </ul>
-                  )}
+                  ) :
+                    (
+
+
+                      //à changer !!!!!!!!!
+                      //si le user n'existe pas, il doit remplir le userform
+                      <>
+                        {/* <Redirect push to="./userform" />; */}
+
+                      </>
+
+                    )}
                 </>
               )}
             />
-            <Route path="/location/:id" component={LocationDetails} />
+
           </Switch>
         </BrowserRouter>
       </header>
---
+++ src/UserForm.js
@@ -0,0 +1,130 @@
+import React, { useState, useContext, useEffect } from 'react';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
+import './App.css';
+
+class UserForm extends React.Component {
+    constructor() {
+      super();
+      this.state = {
+        newUser: this.emptyUser,
+      };
+      /* Add a ref for the title text input */
+      // this.titleInputRef = React.createRef();
+    }
+  
+    /* Emtpy book used in initial state and for resetting the form */
+    emptyUser = { firstname: '', lastname: '', email: '', phone: '', isCreator: 'false', ref_auth:'' };
+  
+    /* Form input change handler */
+    handleFormInputChange = (event) => {
+
+      const target = event.target;
+      const value = target.value;
+      const name = target.name;
+  
+      /*
+      Update state dynamically by spreading the existing
+      newBook object (...prevState.newBook) and overriding
+      the property based on the input name ([name]: value)
+      The second form of setState is used, as we are
+      basing the new value on the previous state
+       */
+      this.setState((prevState) => ({
+        newUser: { ...prevState.newUser, [name]: value },
+      }));
+    };
+  
+    /* Form submission handler */
+    handleFormSubmit = async (event) => {
+      /* Prevent the form submission from reloading the page */
+      event.preventDefault();
+  
+      /* We now call the server to create our new book in the DB */
+      /* Method is POST (for creation of resources)              */
+      /* Content-Type header is set to application/json          */
+      /* The body is the newBook object, stringified as JSON     */
+      // let newUserResponse = await fetch(process.env.REACT_APP_BOOKS_URL, {
+      //   method: 'POST',
+      //   headers: {
+      //     'Content-Type': 'application/json',
+      //   },
+      //   body: JSON.stringify(this.state.newUser),
+      // });
+  
+      /* Get the response body, parsed from JSON */
+      let newUser = this.emptyUser;
+  
+      /* Call the "add book" function that is passed as a prop */
+      this.props.addUser(newUser);
+  
+      /* Reset the new book state */
+      // this.resetUser();
+  
+      /* Focus on the book title after adding a new book */
+      //this.focusBookTitle();
+    };
+  
+    /* Method for focusing on the book title, using the created ref */
+    // focusBookTitle = (event) => {
+    //   /* Use "current" to access the DOM element linked to the ref */
+    //   /* and use the browser API method "focus"                    */
+    //   this.titleInputRef.current.focus();
+    // };
+  
+    /* Reset the new book object */
+    resetNewUser = () => {
+      this.setState({ newUser: this.emptyUser });
+    };
+  
+    render() {
+      return (
+        <>
+          {/* Render a form allowing to add a new book to the list */}
+          <h2>Register yourself !</h2>
+          <form onSubmit={this.handleFormSubmit} className="NewUser-Form">
+            {/* All inputs have been replaced with FormInput components */}
+            <input
+              /* Link the created ref to the title input */
+              fieldRef={this.titleInputRef}
+              type="text"
+              name="title"
+              value={this.state.newUser.firstname}
+              onChange={this.handleFormInputChange}
+              placeholder="Title"
+            />
+            <input
+              type="text"
+              name="author"
+              value={this.state.newUser.lastname}
+              onChange={this.handleFormInputChange}
+              placeholder="Author"
+            />
+            <input
+              type="number"
+              name="year"
+              value={this.state.newUser.email}
+              onChange={this.handleFormInputChange}
+              placeholder="Year of Publication"
+            />
+            <input
+              type="number"
+              name="pages"
+              value={this.state.newUser.phone}
+              onChange={this.handleFormInputChange}
+              placeholder="Number of Pages"
+            />
+            <input
+              type="text"
+              name="cover"
+              value={this.state.newUser.ref_auth}
+              onChange={this.handleFormInputChange}
+              placeholder="ref_auth"
+            />
+            <button type="submit">Register</button>
+          </form>
+        </>
+      );
+    }
+  }
+
+  export default UserForm;
