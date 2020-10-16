9f8fd324f6b3891cad3b2e20b6f2c528a1035c6e
CAHevs <48435770+CAHevs@users.noreply.github.com>
2020-10-16 15:13:26 +0200

    Added Autocomplet for the city

--- src/App.js
+++ src/App.js
@@ -1,192 +1,208 @@
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
 import "./App.css";
-import { useAuth0 } from "@auth0/auth0-react";
+import {useAuth0} from "@auth0/auth0-react";
 import request from "./utils/request";
 import endpoints from "./endpoints";
 import Loading from "./components/Loading";
-import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
+import {BrowserRouter, Link, Switch, Route, Redirect} from "react-router-dom";
+import LocationDetails from "./pages/LocationDetails";
 import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css'
-import UserForm from "./UserForm";
-import ProtectedRoute from "./components/ProtectedRoute";
-import NavBar from "./components/nav-bar";
-import { Navbar, Nav, NavDropdown } from 'react-bootstrap'
-
-let userAuthenticated;
-
-class LocationsList extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-}
-
-class OpenSundayMap extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      selectedDate: null,
-    }
-  }
-
-  render() {
-    return (
-      <>
-        <h1>Hello map ?</h1>
-      </>
-    );
-  }
-}
-
-
-class Home extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      selectedDate: null,
-    }
-  }
-  //Get the city from the user's localization
-  city = "Sion";
-
-  render() {
-    return (
-      <>
-        <h1>Welcome, login then select a town and a date</h1>
-
-        <input
-          //fieldRef={this.titleInputRef}
-          type="text"
-          name="city"
-          value={this.city}
-        // onChange={this.handleFormChange}
-        //placeholder="Title"
-        >
-        </input>
-
-
-        <DatePicker
-          selected={this.state.selectedDate}
-          onChange={this.state.selectedDate = this.date}
-          filterDate={date => date.getDay() == 0}
-          //filterDate={sunHolidDays}
-          minDate={new Date()}
-          placeholderText="Select a sunday or holiday"
-        />
-        <ul className="Map">
-          <Link
-            className="App-Map"
-            to="/Map">
-            <button>
-              map me
-            </button>
-
-          </Link>
-        </ul>
-      </>
-    );
-  }
-}
-
+import 'react-datepicker/dist/react-datepicker.css';
+import UserForm from "./UserForm"
+import MaterialCore from '@material-ui/core';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import TextField from "@material-ui/core/TextField";
 
 function App() {
-  let [locations, setLocations] = useState([]);
+    let [locations, setLocations] = useState([]);
 
-  //Authentification with Auth0
-  let {
-    loading,
-    loginWithRedirect,
-    logout,
-    getAccessTokenSilently,
-    isAuthenticated,
-    user,
-  } = useAuth0();
+    let [userAuthenticated, setUserAuthenticated] = useState([]);
 
+    let [cities, setCities] = useState([]);
 
-  let checkAuthentication = async (e) => {
-    userAuthenticated = await request(
-      `${process.env.REACT_APP_SERVER_URL}${endpoints.user}/getauthenticateduser/${user.sub}`,
-      getAccessTokenSilently,
-      loginWithRedirect);
+    const [selectedDate, setSelectedDate] = useState(null);
 
-    setLocations(await request(
-      `${process.env.REACT_APP_SERVER_URL}${endpoints.location}`,
-      getAccessTokenSilently,
-      loginWithRedirect
-    ));
+    //Authentification with Auth0
+    let {
+        loading,
+        loginWithRedirect,
+        logout,
+        getAccessTokenSilently,
+        isAuthenticated,
+        user,
+    } = useAuth0();
 
-  }
+    useEffect(() => {
+        async function fetchCities() {
+            await getAllCities();
+        }
 
-  //Login button with authentification
-  let handleLoginClick = async (e) => {
-    e.preventDefault();
-    await loginWithRedirect();
-    await checkAuthentication();
+        async function fetchAuthenticatedUser(){
+            await checkAuthentication();
+        }
 
-  };
+        fetchCities();
 
-  let handleLogoutClick = async (e) => {
-    e.preventDefault();
-    /*
+    }, [])
+
+    //Get the city from the user's localization
+    navigator.geolocation.getCurrentPosition(function(position){
+        console.log("Latitude is : ", position.coords.latitude);
+        console.log("Longitude is : ", position.coords.longitude);
+    });
+
+    let checkAuthentication = async (e) => {
+        setUserAuthenticated(await request(
+            `${process.env.REACT_APP_SERVER_URL}${endpoints.user}/GetAuthenticatedUser/${user.sub}`,
+            getAccessTokenSilently,
+            loginWithRedirect
+            )
+        )
+    }
+
+    //Login button with authentification
+    let handleLoginClick = async (e) => {
+        e.preventDefault();
+        await loginWithRedirect();
+        await checkAuthentication();
+    };
+
+    let getAllCities = async (e) => {
+
+        let cities = await request(
+            `${process.env.REACT_APP_SERVER_URL}${endpoints.city}`,
+            getAccessTokenSilently,
+            loginWithRedirect
+        );
+        setCities(cities)
+    }
+
+    function AutoCompleteCity() {
+        return (
+            <>
+                <Autocomplete
+                    freeSolo
+                    id="combo-box-demo"
+                    options={cities}
+                    getOptionLabel={(city) => city.name}
+                    style={{ width: 300 }}
+                    renderInput={(params) => <TextField {...params} label="City" variant="outlined" />}
+                />
+                {//cities.length > 0 &&
+            //    <select>
+            //        {cities.map(city => (
+             //           <option key={city.id} value={city.id}>{city.name} {city.code}</option>
+             //       ))}
+             //   </select>
+                }</>
+        )
+    }
+
+    let handleLogoutClick = async (e) => {
+        e.preventDefault();
+        /*
     returnTo parameter is necessary because multiple apps use the same authentication backend
     */
-    logout({ returnTo: window.location.origin });
-  };
+        logout({returnTo: window.location.origin});
+    };
 
-  if (loading) {
-    return <Loading />;
-  }
+    if (loading) {
+        return <Loading/>;
+    }
 
-  return (
-    <div className="App">
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Brand href="/">Home Sunday</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="Map">Map</Nav.Link>
-            <Nav.Link href="UserForm">Register</Nav.Link>
-            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-          {isAuthenticated ? (
-            /*If the user is authenticated*/
-            <a
-              className="App-link Logout-link"
-              href="#"
-              onClick={handleLogoutClick}
-            >Logout
-            </a>
+    let handleFormSubmit = async (e) => {
+        console.log("map me")
+    }
 
-          ) :
-            //if the user isn't authenticated */
-            <a className="App-link Logout-link"
-              href="#"
-              onClick={handleLoginClick}
-            >Login
-              </a>
-}
-        </Navbar.Collapse>
-      </Navbar>
+    let findNearMe = async (e) => {
 
-      <BrowserRouter>
-        <header className="App-header">
-        </header>
-        <div className="App-body">
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <ProtectedRoute path="/Map" component={OpenSundayMap} />
-            <ProtectedRoute exact path="/UserForm" component={UserForm} />
-          </Switch>
+    }
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                {userAuthenticated  ? (
+                        /*If the user is authenticated*/
+                        <a
+                            className="App-link Logout-link"
+                            href="#"
+                            onClick={handleLogoutClick}
+                        >Logout
+                        </a>
+
+                    ) :
+                    /*if the user isn't authenticated */
+                    <a className="App-link Logout-link"
+                       href="#"
+                       onClick={handleLoginClick}
+                    >Login
+                    </a>
+                }
+
+
+            </header>
+            <body className="App-body">
+            <h1>Welcome, login then select a town and a date</h1>
+
+            {userAuthenticated && <>
+                <AutoCompleteCity/>
+
+
+                <DatePicker
+                    selected={selectedDate}
+                    onChange={date => setSelectedDate(date)}
+                    filterDate={date => date.getDay() == 0}
+                    //filterDate={sunHolidDays}
+                    minDate={new Date()}
+                    placeholderText="Select a sunday or holiday"
+                />
+            </>
+            }
+
+
+            <BrowserRouter>
+                <Switch>
+                    <Route
+                        path="/"
+                        exact
+                        render={() => (
+                            <>
+                                {/* user != null
+                                ici on vérifie que le user exite dans notre db */}
+                                {userAuthenticated ? (
+                                        <ul className="Map">
+                                            <Link
+                                                className="App-Map"
+                                                to="/Map"
+                                                //onClick={}
+                                            >
+                                                <button onClick={handleFormSubmit}>
+                                                    {userAuthenticated} map me
+                                                </button>
+
+                                                <button onClick={findNearMe}>
+                                                    {userAuthenticated} Near me
+                                                </button>
+
+                                            </Link>
+                                        </ul>
+                                    ) :
+                                    (
+                                        <>
+                                            <Route path="/newUser"/>
+                                            <UserForm/>
+                                        </>
+
+                                    )}
+                            </>
+                        )}
+                    />
+
+                </Switch>
+            </BrowserRouter>
+            </body>
         </div>
-      </BrowserRouter>
-
-    </div>
-  );
+    );
 }
 
 export default App;
