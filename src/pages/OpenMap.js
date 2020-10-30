8d78776bf5ba083df001b8b4e9cfcd414bd74545
Alexandre <alex@mbpdealexandre-1.home>
2020-10-30 11:35:03 +0100

    OpenMap updated with share email link

--- src/pages/OpenMap.js
+++ src/pages/OpenMap.js
@@ -12,15 +12,7 @@ import { UserContext } from "../utils/UserContext"
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import TextField from "@material-ui/core/TextField";
 import Loading from '../components/Loading';
-import {
-  EmailShareButton,
-  FacebookShareButton,
-  OKShareButton,
-  FacebookIcon,
-  EmailIcon,
-} from "react-share";
-import { ShareButton } from "react-custom-share";
-import FaFacebook from "react-icons/lib/fa/facebook";
+import { EmailIcon } from "react-share";
 import { Button } from '@material-ui/core';
 
 function OpenMap(props) {
@@ -41,19 +33,16 @@ function OpenMap(props) {
 
   const userContext = useContext(UserContext);
 
-
   // Get the position from the props given from Home page
   navigator.geolocation.getCurrentPosition(async function (position) {
 
     //Set the userContext with lat and lgn from navigator
     userContext.setUserPosition([position.coords.latitude, position.coords.longitude]);
 
-    //If the user doesn't want to give his loocation, set it at Sion, Valais
-  }, () => { userContext.setUserPosition([46.2333, 7.35]); });
+    //If the user doesn't want to give his loocation, set it null
+  }, () => { userContext.setUserPosition(null); });
 
-  // userContext.setUserPosition([46.25, 7.15]);
-
-  let grenIcon = L.icon({
+  let redIcon = L.icon({
     iconUrl: redPin,
     shadowUrl: leafShadow,
     iconSize: [38, 95], // size of the icon
@@ -62,17 +51,6 @@ function OpenMap(props) {
     shadowAnchor: [4, 62],  // the same for the shadow
     popupAnchor: [-3, -76]
   });
-
-
-  const shareButtonProps = {
-    url: "https://github.com/greglobinski/react-custom-share",
-    network: "Facebook",
-    text: "Give it a try - react-custom-share component",
-    longtext:
-      "Social sharing buttons for React. Use one of the build-in themes or create a custom one from the scratch."
-  };
-
-
 
   return (
     <>
@@ -87,50 +65,47 @@ function OpenMap(props) {
         renderInput={(params) => <TextField {...params} label="City" variant="outlined" />}
       />
 
-      {userContext.userPosition === null ? <Loading></Loading>
-        :
-        <Map
-          className="map"
+      <Map
+        className="map"
 
-          //if the user doesn't give his location, the map's center is by default at Sion, Valais
-          // center={userContext.userPosition === null ? [46.2333, 7.35] : userContext.userPosition}
-          center={userContext.userPosition}
-          zoom={15}
-        >
-          <TileLayer
-            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
-          />
-          
-          {
+        //if the user doesn't give his location, the map's center is by default at Sion, Valais
+        center={userContext.userPosition === null ? [46.2333, 7.35] : userContext.userPosition}
+        zoom={15}
+      >
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+        />
 
-            locations.map((loc, index) =>
+        {
 
-              //Get latitude and longitude from each location and display it in a Marker on the map
-              <Marker key={`marker-${index}`} position={[loc.lat, loc.lng]}>
-                <Popup>
-                  <span>
+          locations.map((loc, index) =>
 
-                    {loc.name} <br /> {loc.type.description} <br /> <EmailIcon size={32} round={true} /> <br />
-                    {/* {console.log("URL from locations: "+loc.Url.url)} */}
-                  </span>
-                </Popup>
-              </Marker>
+            //Get latitude and longitude from each location and display it in a Marker on the map
+            <Marker key={`marker-${index}`} position={[loc.lat, loc.lng]}>
+              <Popup>
+                <span>
 
+                  {loc.name} <br /> {loc.type.description} <br />
+                  <EmailIcon size={25} round={true}> </EmailIcon>
+                  <a className="video-email_button button-hover" href={"mailto:?subject=I wanted you to see this site&body=Check out this link " + loc.url} title="Share viaEmail">
+                    <span className="video-email_button-text">Share me</span>
+                  </a>
+                </span>
+              </Popup>
+            </Marker>
+          )}
 
-
-            )}
-
+        {/* Display the redPin form user's location if it exists */}
+        {!(userContext.userPosition === null) &&
           <Marker
             position={userContext.userPosition}
-            icon={grenIcon}
+            icon={redIcon}
           >
             <Popup>You are here</Popup>
           </Marker>
-
-          <button>FIND ME</button>
-        </Map>
-      }
+        }
+      </Map>
     </>
   );
 }
